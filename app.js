3f91249ef28b4fe767cf201ff5640b4ce02d367d
aguilanbon <aguilanbon@gmail.com>
2022-05-27 20:05:54 +0800

    added testimonial and carousel

--- app.js
+++ app.js
@@ -23,3 +23,25 @@ const animateText = () => {
 }
 
 animateText()
+
+
+
+const btns = document.querySelectorAll('[data-carousel-button]')
+
+
+btns.forEach(button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1
+        const slides = button.closest('[data-carousel]').querySelector('[data-slides')
+
+        const activeSlide = slides.querySelector('[data-active]')
+
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+
+        if(newIndex<0) newIndex = slides.children.length - 1
+        if(newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
